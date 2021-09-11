45349b9cee36ac77d7d3dd4e5a8ba83e9b8a5e39
nyarla <nyarla@kalaclista.com>
2021-09-12 06:53:12 +0900

    new: added deadbeef-file-browser-plugin

--- pkgs.nix
+++ pkgs.nix
@@ -5,20 +5,15 @@ in {
   ibus-skk = require ./pkgs/ibus-skk { };
   libvterm-neovim-mlterm = require ./pkgs/libvterm-neovim-mlterm { };
   wcwidth-cjk = require ./pkgs/wcwidth-cjk { };
+  deadbeef-file-browser-plugin =
+    require ./pkgs/deadbeef-file-browser-plugin { };
 
   # fonts
   nerdfont-symbols-2048 = require ./pkgs/nerdfont-symbols/2048.nix { };
   noto-fonts-jp = require ./pkgs/noto-fonts-jp { };
 
   # modified
-  # mlterm = super.mlterm.overrideAttrs (old: rec {
-  #   src = super.fetchurl {
-  #     url =
-  #       "https://github.com/arakiken/mlterm/archive/refs/heads/master.tar.gz";
-  #     sha256 = "10bh0azg5dxpr4c4xl3r9yk56dh1scncv35clxa3vzpmk96k1dqc";
-  #   };
-  # });
-  # neovim-unwrapped = super.neovim-unwrapped.override {
-  #   libvterm-neovim = self.libvterm-neovim-mlterm;
-  # };
+  calibre = super.calibre.overrideAttrs (old: rec {
+    buildInputs = [ super.python3Packages.pycrypto ] ++ old.buildInputs;
+  });
 }
