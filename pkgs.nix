c34dd2327c6ec85df9be29c6fee641d2835cd011
nyarla <nyarla@kalaclista.com>
2021-09-28 14:38:12 +0900

    update: enable parsec-bin and wineasio

--- pkgs.nix
+++ pkgs.nix
@@ -2,11 +2,13 @@ self: super:
 let require = path: args: super.callPackage (import path) args;
 in {
   # new packages
-  ibus-skk = require ./pkgs/ibus-skk { };
-  libvterm-neovim-mlterm = require ./pkgs/libvterm-neovim-mlterm { };
-  wcwidth-cjk = require ./pkgs/wcwidth-cjk { };
   deadbeef-file-browser-plugin =
     require ./pkgs/deadbeef-file-browser-plugin { };
+  ibus-skk = require ./pkgs/ibus-skk { };
+  libvterm-neovim-mlterm = require ./pkgs/libvterm-neovim-mlterm { };
+  parsec-bin = require ./pkgs/parsec-bin { };
+  wcwidth-cjk = require ./pkgs/wcwidth-cjk { };
+  wineasio = require ./pkgs/wineasio { };
 
   # fonts
   nerdfont-symbols-2048 = require ./pkgs/nerdfont-symbols/2048.nix { };
