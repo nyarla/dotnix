b741d181c08e3c9a2a4754009060f7dafcfa2335
nyarla <nyarla@kalaclista.com>
2021-10-15 11:39:46 +0900

    new: add Shell cpan module as perlPackages.Shell

--- pkgs.nix
+++ pkgs.nix
@@ -1,7 +1,6 @@
 self: super:
 let require = path: args: super.callPackage (import path) args;
 in {
-  # new packages
   deadbeef-file-browser-plugin =
     require ./pkgs/deadbeef-file-browser-plugin { };
   ibus-skk = require ./pkgs/ibus-skk { };
@@ -19,4 +18,20 @@ in {
   calibre = super.calibre.overrideAttrs (old: rec {
     buildInputs = [ super.python3Packages.pycrypto ] ++ old.buildInputs;
   });
+
+  perlPackages = (with super.perlPackages; {
+    Shell = buildPerlPackage {
+      pname = "Shell";
+      version = "0.73";
+      src = super.fetchurl {
+        url = "mirror://cpan/authors/id/F/FE/FERREIRA/Shell-0.73.tar.gz";
+        sha256 =
+          "f7dbebf65261ed0e5abd0f57052b64d665a1a830bab4c8bbc220f235bd39caf5";
+      };
+      meta = {
+        description = "Run shell commands transparently within perl";
+        license = with super.lib.licenses; [ artistic1 gpl1Plus ];
+      };
+    };
+  }) // super.perlPackages;
 }
