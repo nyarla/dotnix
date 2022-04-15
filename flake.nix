96769d55a6449df7e3258b31e031bbbc23e64773
nyarla <nyarla@kalaclista.com>
2022-04-15 11:37:35 +0900

    chore(flake): move some packages to dotfiles or nixos-configuration repository

--- flake.nix
+++ flake.nix
@@ -38,10 +38,6 @@
             [ "com.bitwig.BitwigStudio.desktop" ] old.installPhase);
         });
 
-        calibre = super.calibre.overrideAttrs (old: rec {
-          buildInputs = [ super.python3Packages.pycrypto ] ++ old.buildInputs;
-        });
-
         whipper = super.whipper.overrideAttrs (old: rec {
           postPatch = ''
             sed -i 's|cd-paranoia|${super.cdparanoia}/bin/cdparanoia|g' whipper/program/cdparanoia.py
@@ -63,43 +59,6 @@
             };
           };
         }) // super.perlPackages;
-
-        # coin
-        ethminer = (super.ethminer.override {
-          stdenv = super.llvmPackages_13.stdenv;
-          cudatoolkit = super.cudatoolkit_11_5;
-        }).overrideAttrs (old: rec {
-          version = "1.9.2";
-          src = super.fetchFromGitHub {
-            owner = "danieleftodi";
-            repo = "ethminer";
-            rev = "59e063c57d44b3de5393b81f92d89b76d86107a3";
-            sha256 = "1ra1jlc8s3r12qzqkbqzf8646cbj219mqdms04wdzaya2af444h6";
-            fetchSubmodules = true;
-          };
-          postPatch = ''
-            sed -i 's|jsoncpp_static|jsoncpp|' libpoolprotocols/CMakeLists.txt
-            sed -i 's|-Wall|-Wall -Ofast -funroll-loops|g' cmake/EthCompilerSettings.cmake
-          '';
-        });
-        nsfminer = (require ./pkgs/nsfminer {
-          stdenv = super.llvmPackages_13.stdenv;
-          cudatoolkit = super.cudatoolkit_11_5;
-        }).overrideAttrs (old: rec {
-          postPatch = old.preConfigure + ''
-            sed -i 's/set(CMAKE_CXX_FLAGS "''${CMAKE_CXX_FLAGS} -stdlib=libstdc++ -fcolor-diagnostics -Qunused-arguments")//' cmake/EthCompilerSettings.cmake
-            sed -i 's|-Wall|-Wall -Ofast -funroll-loops|g' cmake/EthCompilerSettings.cmake
-          '';
-        });
-        xmrig = (super.xmrig.override {
-          stdenv = super.llvmPackages_13.stdenv;
-        }).overrideAttrs (old: rec {
-          preConfigure = ''
-            sed -i 's/-Ofast/-Ofast -funroll-loops/g' cmake/flags.cmake
-          '';
-
-        });
-
       };
   };
 }
